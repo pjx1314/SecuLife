62d25decbfad42170613e3b9eddba67bacb59bc7
pjx1314 <88777966+pjx1314@users.noreply.github.com>
2025-04-29 17:38:04 +0800

    Update auto-pr.ps1

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -1,6 +1,6 @@
 param(
     # overwrite upstream param
-    [String]$upstream = "pjx1314/Buckets:main"
+    [String]$upstream = "pjx1314/SecuLife:main"
 )
 
 if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Convert-Path (scoop prefix scoop) }
